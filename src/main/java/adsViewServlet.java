b8e9a3ac5fd6e10b6efc50133f1b3855414dc64f
kuwan barnes <Kuwan.barne@gmail.com>
2021-03-10 11:58:55 -0600

    loginservlet

--- src/main/java/adsViewServlet.java
+++ src/main/java/adsViewServlet.java
@@ -17,6 +17,12 @@ public class adsViewServlet extends HttpServlet {
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
+        if (request.getMethod().equalsIgnoreCase("post")) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+        response.sendRedirect("/profile");
+        }
+        }
     }
 }
